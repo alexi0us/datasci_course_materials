e929da67eb00e990255ded576d4554254e510c46
Alexis Kousounadis <koussou@gmail.com>
2014-07-08 16:29:07 +0100

    wq!removing my credentials

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -3,14 +3,14 @@ import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
+api_key = ""
+api_secret = ""
+access_token_key = ""
+access_token_secret = ""
 
 _debug = 0
 
-oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_token = oauth.Token(key=access_token_key, secret=access_token_secret)
 oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
 
 signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
@@ -18,7 +18,7 @@ signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
 http_method = "GET"
 
 
-http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+http_handler = urllib.HTTPHandler(debuglevel=_debug)
 https_handler = urllib.HTTPSHandler(debuglevel=_debug)
 
 '''
@@ -29,7 +29,7 @@ def twitterreq(url, method, parameters):
   req = oauth.Request.from_consumer_and_token(oauth_consumer,
                                              token=oauth_token,
                                              http_method=http_method,
-                                             http_url=url, 
+                                             http_url=url,
                                              parameters=parameters)
 
   req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
@@ -45,6 +45,9 @@ def twitterreq(url, method, parameters):
   opener = urllib.OpenerDirector()
   opener.add_handler(http_handler)
   opener.add_handler(https_handler)
+  
+  proxy = urllib.ProxyHandler()
+  opener = urllib.build_opener(proxy)
 
   response = opener.open(url, encoded_post_data)
 
@@ -59,3 +62,5 @@ def fetchsamples():
 
 if __name__ == '__main__':
   fetchsamples()
+
+
